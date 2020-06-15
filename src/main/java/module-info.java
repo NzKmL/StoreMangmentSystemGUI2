72086fa433f597740211a047ea643e7f0846e0c0
Kamil Matejak <matejak.kamil@gmail.com>
2020-06-15 22:30:13 +0200

    Add SLF4J logger

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module pl.nzkml {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.slf4j;
 
     opens pl.nzkml.views to javafx.fxml;
     opens pl.nzkml.controllers to javafx.fxml;
--- src/main/java/pl/nzkml/StoreManagmentSystemAPP.java
+++ src/main/java/pl/nzkml/StoreManagmentSystemAPP.java
@@ -9,16 +9,26 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * JavaFX App
  */
 public class StoreManagmentSystemAPP extends Application {
 
+    Logger logger = LoggerFactory.getLogger(StoreManagmentSystemAPP.class);
+
     @Override
     public void start(Stage stage) throws IOException {
+        logger.info("Application Start");
         SMSSceneManager.getInstance().firstScene(stage);
 
+    }
+
+    @Override
+    public void stop() throws Exception {
+        logger.info("Application stop");
+        super.stop();
     }
 
     public static void main(String[] args) {
