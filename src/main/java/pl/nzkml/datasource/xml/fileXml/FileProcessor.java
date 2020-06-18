1780a1fc952c886876d4853f3a0ddf4062edd84d
Kamil Matejak <matejak.kamil@gmail.com>
2020-06-18 23:09:04 +0200

    add User.xml file, file operation class  FileProcessor.java and ApplicationProperties.java

---
+++ src/main/java/pl/nzkml/datasource/xml/fileXml/FileProcessor.java
@@ -0,0 +1,29 @@
+package pl.nzkml.datasource.xml.fileXml;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileProcessor {
+
+    public String readFile(String fileName) throws FileNotFoundException{
+            File file = new File(fileName);
+            Scanner scaner = new Scanner(file);
+            StringBuilder sb = new StringBuilder();
+
+            while (scaner.hasNextLine()) {
+                sb.append(scaner.nextLine());
+            }
+
+            scaner.close();
+            return sb.toString();
+    }
+    public void writeToFile(String fileName, String text) throws IOException {
+        FileWriter myWriter = new FileWriter(fileName);
+        myWriter.write(text);
+        myWriter.close();
+    }
+
+}
